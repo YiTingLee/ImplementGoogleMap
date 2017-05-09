170bff182f7f8c63a52b15bf1c42d4020da5081c
YiTingLee <ranye0719@gmail.com>
2017-05-09 20:02:33 +0800

    add geolocation but map is not working

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
+import SimpleMapExample from './google_map';
+
+
 
 export default class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {lat:0.0,lon:0.0};
+
+    navigator.geolocation.getCurrentPosition((success) =>{
+      this.setState({lat:success.coords.latitude,lon:success.coords.longitude});
+      console.log(this.state);
+    }, (error) =>{
+      console.log("Your browser doesn't support geolocation.");
+    });
+
+
+  }
+
+
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <div>lat={this.state.lat}</div>
+        <div>lng={this.state.lon}</div>
+        <SimpleMapExample />
+        END
+      </div>
     );
   }
 }
---
+++ src/components/google_map.js
@@ -0,0 +1,32 @@
+import  React,{Component} from "react";
+
+import {withGoogleMap,GoogleMap} from "react-google-maps";
+
+/*
+ * Sample From: https://developers.google.com/maps/documentation/javascript/examples/map-simple
+ */
+const SimpleMapExampleGoogleMap = withGoogleMap(props => (
+  <GoogleMap
+    defaultZoom={12}
+    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+  />
+));
+
+/*
+ * Add <script src="https://maps.googleapis.com/maps/api/js"></script> to your HTML to provide google.maps reference
+ */
+export default class SimpleMapExample extends Component {
+
+  render() {
+    return (
+      <SimpleMapExampleGoogleMap
+        containerElement={
+           <div style={{height: '100%', width: '100%'}} />
+        }
+        mapElement={
+           <div style={{height: '100%', width: '100%'}} />
+        }
+      />
+    );
+  }
+}
