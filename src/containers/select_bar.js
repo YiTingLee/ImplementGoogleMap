523f270f17c911efa4eac498d9332ddba15fe468
YiTingLee <ranye0719@gmail.com>
2017-05-11 19:17:30 +0800

    add select onchange event

--- src/containers/select_bar.js
+++ src/containers/select_bar.js
@@ -1,9 +1,14 @@
 import React,{Component} from 'react';
 
 export default class SelectBar extends Component{
+
+  onSelectChange(event){
+    console.log(event.target.value);
+  }
+
   render(){
     return (
-      <select>
+      <select onChange={this.onSelectChange}>
         <option value="ntut">NTUT</option>
         <option value="station">台北車站</option>
         <option value="taipei101">台北101</option>
